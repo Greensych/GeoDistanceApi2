091a403780bf88a2e49aab759f601215898c735c
Greensych <alternative191@yandex.ru>
2025-12-26 12:20:26 +0300

    add required attributes

--- src/GeoDistanceApi/Models/DistanceRequest.cs
+++ src/GeoDistanceApi/Models/DistanceRequest.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GeoDistanceApi.Models;
 
 public class DistanceRequest
 {
+    [Required]
     public GeoCoordinate? From { get; set; }
+
+    [Required]
     public GeoCoordinate? To { get; set; }
 }
