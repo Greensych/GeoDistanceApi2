59457cc3b296bea20e07d773fc4498182858021d
Greensych <alternative191@yandex.ru>
2025-12-26 12:20:39 +0300

    configure swagger

--- src/GeoDistanceApi/Program.cs
+++ src/GeoDistanceApi/Program.cs
@@ -1,9 +1,23 @@
+using GeoDistanceApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<DistanceCalculator>();
+
+// добавляем swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
