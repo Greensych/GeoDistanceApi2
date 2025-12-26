08bf1b7c9fd3a5af84300ec69a7dee8da4e73969
Greensych <alternative191@yandex.ru>
2025-12-26 12:20:25 +0300

    add validation to coordinates

--- src/GeoDistanceApi/Models/GeoCoordinate.cs
+++ src/GeoDistanceApi/Models/GeoCoordinate.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GeoDistanceApi.Models;
 
 public class GeoCoordinate
 {
+    [Range(-90, 90)]
     public double Latitude { get; set; }
+
+    [Range(-180, 180)]
     public double Longitude { get; set; }
 }
